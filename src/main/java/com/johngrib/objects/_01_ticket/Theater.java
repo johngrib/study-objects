0b8c224da21ed0d010821c3031960eec63f08b4d
John Grib <johngrib82@gmail.com>
2020-03-01 16:31:19 +0900

    관람객을 맞이하는 로직을 구현하라

---
+++ src/main/java/com/johngrib/objects/_01_ticket/Theater.java
@@ -0,0 +1,29 @@
+package com.johngrib.objects._01_ticket;
+
+/** 극장. */
+public class Theater {
+  private TicketSeller ticketSeller;
+
+  public Theater(TicketSeller ticketSeller) {
+    this.ticketSeller = ticketSeller;
+  }
+
+  /**
+   * 관람객을 맞이한다.
+   *
+   * @param audience 관람객
+   */
+  public void enter(Audience audience) {
+    if (audience.getBag().hasInvitation()) {
+      // 초대장이 있는 관람객의 경우
+      Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+      audience.getBag().setTicket(ticket);
+    } else {
+      // 초대장이 없는 관람객의 경우
+      Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+      audience.getBag().minusAmount(ticket.getFee());
+      ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+      audience.getBag().setTicket(ticket);
+    }
+  }
+}
