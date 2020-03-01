ba97e71c9cef33651f338ccb9df446f7e3062870
John Grib <johngrib82@gmail.com>
2020-03-01 16:23:59 +0900

    티켓 판매원을 구현하라

---
+++ src/main/java/com/johngrib/objects/_01_ticket/TicketSeller.java
@@ -0,0 +1,14 @@
+package com.johngrib.objects._01_ticket;
+
+import lombok.Getter;
+
+/** 티켓 판매원. */
+public class TicketSeller {
+  @Getter
+  private TicketOffice ticketOffice;
+
+  /** 티켓 판매원은 자신이 일하는 매표소를 알고 있어야 한다. */
+  public TicketSeller(TicketOffice ticketOffice) {
+    this.ticketOffice = ticketOffice;
+  }
+}
