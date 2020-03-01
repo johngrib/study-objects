a06cb5f2608911f3b89dfa284c2fe3e152d14a40
John Grib <johngrib82@gmail.com>
2020-03-01 22:43:44 +0900

    금액을 의미하는 Money 클래스를 구현하라

---
+++ src/main/java/com/johngrib/objects/_02_movie/Money.java
@@ -0,0 +1,42 @@
+package com.johngrib.objects._02_movie;
+
+import java.math.BigDecimal;
+
+/** 금액. */
+public class Money {
+  public static final Money ZERO = Money.wons(0);
+
+  private final BigDecimal amount;
+
+  public static Money wons(long amount) {
+    return new Money(BigDecimal.valueOf(amount));
+  }
+
+  public static Money wons(double amount) {
+    return new Money(BigDecimal.valueOf(amount));
+  }
+
+  Money(BigDecimal amount) {
+    this.amount = amount;
+  }
+
+  public Money plus(Money amount) {
+    return new Money(this.amount.add(amount.amount));
+  }
+
+  public Money minus(Money amount) {
+    return new Money(this.amount.subtract(amount.amount));
+  }
+
+  public Money times(double percent) {
+    return new Money(this.amount.multiply(BigDecimal.valueOf(percent)));
+  }
+
+  public boolean isLessThan(Money other) {
+    return amount.compareTo(other.amount) < 0;
+  }
+
+  public boolean isGreaterThanOrEqual(Money other) {
+    return amount.compareTo(other.amount) >= 0;
+  }
+}
