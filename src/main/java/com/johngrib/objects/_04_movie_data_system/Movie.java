227dbfb405a73e3d8a2bb9ce07aa2087a0607c97
John Grib <johngrib82@gmail.com>
2020-03-08 22:08:02 +0900

    데이터 중심의 Movie 클래스를 작성하라
    * 기존의 설계와 동일한 부분은 세 가지 뿐이다.
        * title 을 인스턴스 변수로 포함한다.
        * runningTime 을 인스턴스 변수로 포함한다.
        * fee 를 인스턴스 변수로 포함한다.
    * 가장 큰 차이점은 할인 조건 목록(discountConditions)이 포함되어 있다는 것.

---
+++ src/main/java/com/johngrib/objects/_04_movie_data_system/Movie.java
@@ -0,0 +1,34 @@
+package com.johngrib.objects._04_movie_data_system;
+
+import com.johngrib.objects._02_movie.Money;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+
+public class Movie {
+  private String title;
+  private Duration runningTime;
+  @Getter
+  @Setter
+  private Money fee;
+
+  @Setter
+  private List<DiscountCondition> discountConditions;
+
+  public List<DiscountCondition> getDiscountConditions() {
+    return Collections.unmodifiableList(discountConditions);
+  }
+
+  @Getter
+  @Setter
+  private MovieType movieType;
+  @Getter
+  @Setter
+  private Money discountAmount;
+  @Getter
+  @Setter
+  private double discountPercent;
+}
