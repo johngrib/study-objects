b00e0f26bc4c0df40fa3940ca67984a4e9fd9634
John Grib <johngrib82@gmail.com>
2020-03-10 21:56:03 +0900

    개선: 할인조건을 판단하는 isDiscountable을 추가하라

--- src/main/java/com/johngrib/objects/_04_movie_data_system/DiscountCondition.java
+++ src/main/java/com/johngrib/objects/_04_movie_data_system/DiscountCondition.java
@@ -22,4 +22,20 @@ public class DiscountCondition {
   @Getter
   @Setter
   private LocalTime endTime;
+
+  public boolean isDiscountable(DayOfWeek dayOfWeek, LocalTime time) {
+    if (type != DiscountConditionType.PERIOD) {
+      throw new IllegalArgumentException();
+    }
+    return this.dayOfWeek.equals(dayOfWeek)
+            && this.startTime.compareTo(time) <= 0
+            && this.endTime.compareTo(time) >= 0;
+  }
+
+  public boolean isDiscountable(int sequence) {
+    if (type != DiscountConditionType.SEQUENCE) {
+      throw new IllegalArgumentException();
+    }
+    return this.sequence == sequence;
+  }
 }
