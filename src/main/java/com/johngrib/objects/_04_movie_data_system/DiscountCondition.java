68dcc79a578bcd3a093668e238beb0165d49a6e6
John Grib <johngrib82@gmail.com>
2020-03-10 22:18:21 +0900

    개선: 불필요한 Getter, Setter를 삭제하라

--- src/main/java/com/johngrib/objects/_04_movie_data_system/DiscountCondition.java
+++ src/main/java/com/johngrib/objects/_04_movie_data_system/DiscountCondition.java
@@ -1,26 +1,15 @@
 package com.johngrib.objects._04_movie_data_system;
 
 import lombok.Getter;
-import lombok.Setter;
-
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
 public class DiscountCondition {
   @Getter
-  @Setter
   private DiscountConditionType type;
-  @Getter
-  @Setter
   private int sequence;
-  @Getter
-  @Setter
   private DayOfWeek dayOfWeek;
-  @Getter
-  @Setter
   private LocalTime startTime;
-  @Getter
-  @Setter
   private LocalTime endTime;
 
   public boolean isDiscountable(DayOfWeek dayOfWeek, LocalTime time) {
--- src/main/java/com/johngrib/objects/_04_movie_data_system/Screening.java
+++ src/main/java/com/johngrib/objects/_04_movie_data_system/Screening.java
@@ -1,20 +1,12 @@
 package com.johngrib.objects._04_movie_data_system;
 
 import com.johngrib.objects._02_movie.Money;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 public class Screening {
-  @Getter
-  @Setter
   private Movie movie;
-  @Getter
-  @Setter
   private int sequence;
-  @Getter
-  @Setter
   private LocalDateTime whenScreened;
 
   public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
