63de043bb02576f00dc7e22debec475a46ec45f8
John Grib <johngrib82@gmail.com>
2020-03-10 22:10:34 +0900

    개선: 상영 클래스에 요금 계산 메소드를 추가하라

--- src/main/java/com/johngrib/objects/_04_movie_data_system/Screening.java
+++ src/main/java/com/johngrib/objects/_04_movie_data_system/Screening.java
@@ -1,5 +1,6 @@
 package com.johngrib.objects._04_movie_data_system;
 
+import com.johngrib.objects._02_movie.Money;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,4 +16,21 @@ public class Screening {
   @Getter
   @Setter
   private LocalDateTime whenScreened;
+
+  public Money calculateFee(int audienceCount) {
+    switch (movie.getMovieType()) {
+      case AMOUNT_DISCOUNT:
+        if (movie.isDiscountable(whenScreened, sequence)) {
+          return movie.calculateAmountDiscountedFee().times(audienceCount);
+        }
+        break;
+      case PERCENT_DISCOUNT:
+        if (movie.isDiscountable(whenScreened, sequence)) {
+          return movie.calculatePercentDiscountedFee().times(audienceCount);
+        }
+      case NONE_DISCOUNT:
+        return movie.calculateNoneDiscountedFee().times(audienceCount);
+    }
+    return movie.calculateNoneDiscountedFee().times(audienceCount);
+  }
 }
