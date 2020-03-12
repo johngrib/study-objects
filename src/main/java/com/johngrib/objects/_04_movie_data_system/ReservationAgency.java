721db76f04bb3ed82743c538e6801b69539ba544
John Grib <johngrib82@gmail.com>
2020-03-12 21:41:31 +0900

    잘못된 주석을 삭제하라

--- src/main/java/com/johngrib/objects/_04_movie_data_system/ReservationAgency.java
+++ src/main/java/com/johngrib/objects/_04_movie_data_system/ReservationAgency.java
@@ -3,7 +3,6 @@ package com.johngrib.objects._04_movie_data_system;
 import com.johngrib.objects._02_movie.Money;
 
 public class ReservationAgency {
-  /** Movie에 설정된 DiscountCondition 목록을 차례대로 탐색하며 영화의 할인 여부를 판단한다. */
   public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
     Money fee = screening.calculateFee(audienceCount);
     return new Reservation(customer, screening, fee, audienceCount);
