4ec0f1cfbeaffc64e0175f97500692bfaf1ae4f3
John Grib <johngrib82@gmail.com>
2020-03-22 18:55:34 +0900

    실습 대상인 1장 코드를 가져오라

---
+++ src/main/java/com/johngrib/objects/_06_refactoring_01/Audience.java
@@ -0,0 +1,13 @@
+package com.johngrib.objects._06_refactoring_01;
+
+import lombok.Getter;
+
+/** 관람객. */
+public class Audience {
+  @Getter
+  private Bag bag;
+
+  public Audience(Bag bag) {
+    this.bag = bag;
+  }
+}
---
+++ src/main/java/com/johngrib/objects/_06_refactoring_01/Bag.java
@@ -0,0 +1,55 @@
+package com.johngrib.objects._06_refactoring_01;
+
+import lombok.Getter;
+
+/** 관람객이 소지품을 보관할 가방. */
+public class Bag {
+  @Getter
+  private Long amount;
+  private Invitation invitation;
+  private Ticket ticket;
+
+  /**
+   * 이벤트에 당첨되지 않은 관람객. 초대장이 없다.
+   *
+   * @param amount 현금
+   */
+  public Bag(Long amount) {
+    this.amount = amount;
+  }
+
+  /**
+   * 이벤트에 당첨된 관람객. 현금과 초대장이 있다.
+   *
+   * @param amount     현금
+   * @param invitation 초대장
+   */
+  public Bag(Long amount, Invitation invitation) {
+    this.amount = amount;
+    this.invitation = invitation;
+  }
+
+  /** 관람객이 초대장을 갖고 있다면 true 를 리턴한다. */
+  public boolean hasInvitation() {
+    return invitation != null;
+  }
+
+  /** 관람객이 티켓을 갖고 있다면 true 를 리턴한다. */
+  public boolean hasTicket() {
+    return ticket != null;
+  }
+
+  public void setTicket(Ticket ticket) {
+    this.ticket = ticket;
+  }
+
+  /** 현금을 감소시킨다. */
+  public void minusAmount(Long amount) {
+    this.amount -= amount;
+  }
+
+  /** 현금을 증가시킨다. */
+  public void plusAmount(Long amount) {
+    this.amount += amount;
+  }
+}
