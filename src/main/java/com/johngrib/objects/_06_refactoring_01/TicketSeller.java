ac7cb3e8f464996b93bb14702a6fbfe49613b760
John Grib <johngrib82@gmail.com>
2020-03-22 18:59:16 +0900

    enter 메소드의 로직을 TicketSeller로 옮겨라

--- src/main/java/com/johngrib/objects/_06_refactoring_01/TicketSeller.java
+++ src/main/java/com/johngrib/objects/_06_refactoring_01/TicketSeller.java
@@ -11,4 +11,16 @@ public class TicketSeller {
   public TicketSeller(TicketOffice ticketOffice) {
     this.ticketOffice = ticketOffice;
   }
+
+  public void setTicket(Audience audience) {
+    if (audience.getBag().hasInvitation()) {
+      Ticket ticket = ticketOffice.getTicket();
+      audience.getBag().setTicket(ticket);
+    } else {
+      Ticket ticket = ticketOffice.getTicket();
+      audience.getBag().minusAmount(ticket.getFee());
+      ticketOffice.plusAmount(ticket.getFee());
+      audience.getBag().setTicket(ticket);
+    }
+  }
 }
