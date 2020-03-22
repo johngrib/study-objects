472a65b5ce8e3c36b07b7ef80323984ec283f86f
John Grib <johngrib82@gmail.com>
2020-03-22 22:52:01 +0900

    TicketSeller가 디미터 법칙을 준수하게 하라

--- src/main/java/com/johngrib/objects/_06_refactoring_01/Audience.java
+++ src/main/java/com/johngrib/objects/_06_refactoring_01/Audience.java
@@ -10,4 +10,15 @@ public class Audience {
   public Audience(Bag bag) {
     this.bag = bag;
   }
+
+  public long setTicket(Ticket ticket) {
+    if (bag.hasInvitation()) {
+      bag.setTicket(ticket);
+      return 0L;
+    } else {
+      bag.setTicket(ticket);
+      bag.minusAmount(ticket.getFee());
+      return ticket.getFee();
+    }
+  }
 }
--- src/main/java/com/johngrib/objects/_06_refactoring_01/TicketSeller.java
+++ src/main/java/com/johngrib/objects/_06_refactoring_01/TicketSeller.java
@@ -13,14 +13,8 @@ public class TicketSeller {
   }
 
   public void setTicket(Audience audience) {
-    if (audience.getBag().hasInvitation()) {
-      Ticket ticket = ticketOffice.getTicket();
-      audience.getBag().setTicket(ticket);
-    } else {
-      Ticket ticket = ticketOffice.getTicket();
-      audience.getBag().minusAmount(ticket.getFee());
-      ticketOffice.plusAmount(ticket.getFee());
-      audience.getBag().setTicket(ticket);
-    }
+    ticketOffice.plusAmount(
+            audience.setTicket(ticketOffice.getTicket())
+    );
   }
 }
