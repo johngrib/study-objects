fd3aec4f54e9516070245ee0c01b9c84f7d05fb9
John Grib <johngrib82@gmail.com>
2020-03-31 22:52:54 +0900

    상속을 이용해 중복을 제거하고 문제점을 생각하라

--- src/main/java/com/johngrib/objects/_10_call/NightlyDiscountPhone.java
+++ src/main/java/com/johngrib/objects/_10_call/NightlyDiscountPhone.java
@@ -9,41 +9,32 @@ import java.util.List;
 /**
  * 심야 할인 요금제.
  */
-public class NightlyDiscountPhone {
+public class NightlyDiscountPhone extends Phone {
   private static final int LATE_NIGHT_HOUR = 22;
 
   private Money nightlyAmount;
-  private Money regularAmount;
-  private Duration seconds;
   private List<Call> calls = new ArrayList<>();
-  private double taxRate;
 
   public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+    super(regularAmount, seconds);
     this.nightlyAmount = nightlyAmount;
-    this.regularAmount = regularAmount;
-    this.seconds = seconds;
   }
 
-  public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds, double taxRate) {
-    this.nightlyAmount = nightlyAmount;
-    this.regularAmount = regularAmount;
-    this.seconds = seconds;
-    this.taxRate = taxRate;
-  }
-
+  @Override
   public Money calculateFee() {
-    Money result = Money.ZERO;
+    // 부모 클래스의 calculateFee 호출
+    Money result = super.calculateFee();
+
+    Money nightlyFee = Money.ZERO;
 
     for (Call call : calls) {
       if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
-        result = result.plus(
-                nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
-      } else {
-        result = result.plus(
-                regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+        nightlyFee = nightlyFee.plus(getAmount().minus(nightlyAmount).times(
+                call.getDuration().getSeconds() / getSeconds().getSeconds()
+        ));
       }
     }
     // 일반 요금제는 plus 를 쓰는데 여기에서는 minus 를 쓴다.
-    return result.minus(result.times(taxRate));
+    return result.minus(nightlyFee);
   }
 }
