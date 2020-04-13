4a10e2178e91347a7d3cdf252c42cb4bd43a6166
John Grib <johngrib82@gmail.com>
2020-04-13 23:16:37 +0900

    학생들의 이수 여부와 등급별 통계를 함께 출력하게 하라

--- src/main/java/com/johngrib/objects/_12_lecture/GradeLecture.java
+++ src/main/java/com/johngrib/objects/_12_lecture/GradeLecture.java
@@ -1,6 +1,7 @@
 package com.johngrib.objects._12_lecture;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GradeLecture extends Lecture {
   private List<Grade> grades;
@@ -8,5 +9,26 @@ public class GradeLecture extends Lecture {
   public GradeLecture(String title, int pass, List<Grade> grades, List<Integer> scores) {
     super(title, pass, scores);
     this.grades = grades;
+  }
+
+  @Override
+  public String evaluate() {
+    return super.evaluate() + ", " + gradeStatistics();
+  }
+
+  private String gradeStatistics() {
+    return grades.stream()
+            .map(grade -> format(grade))
+            .collect(Collectors.joining(" "));
+  }
+
+  private String format(Grade grade) {
+    return String.format("%s:%d", grade.getName(), gradeCount(grade));
+  }
+
+  private long gradeCount(Grade grade) {
+    return getScores().stream()
+            .filter(grade::include)
+            .count();
   }
 }
